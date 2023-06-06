7a53b8d735f01d921bfb3674bfe3f632a4bce0fc
pritamstech <98208329+PRITAMSLEARNINGACCOUNT@users.noreply.github.com>
2023-06-06 14:31:29 +0530

    Add files via upload

---
+++ CLI_Downloader.py
@@ -0,0 +1,19 @@
+import requests
+from tqdm import tqdm
+import math
+url = input("Enter Your Fucking URL??\n")
+response = requests.get(url, stream=True)
+total_size_in_bytes = float(response.headers["Content-Length"])
+total_size = float("{:.2f}".format(
+    float(response.headers["Content-Length"])*math.pow(10, -9)))
+print(f"The Size Of Your Given File Is {total_size} GB")
+
+splitted_url = url.split("/")
+chunk_size = 128
+with open(f"{splitted_url[-1]}", "wb") as fuck:
+    progress_bar = tqdm(total=total_size_in_bytes, unit="iB", unit_scale=True)
+    for i in response.iter_content(chunk_size):
+        fuck.write(i)
+        progress_bar.update(chunk_size)
+progress_bar.close()
+print("The File Has Been Downloaded Successfully.")
